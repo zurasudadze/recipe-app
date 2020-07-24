704e84be4ff2f7186d032f4f2762fb256b57d42c
zura sudadze <zurasudadze@gmail.com>
2020-07-24 16:01:52 +0400

    routing added

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -28,6 +28,10 @@ export class RecipeService {
   getRecipes() {
     return this.recipes.slice();
   }
+
+  getRecipe(id: number) {
+    return this.recipes[id];
+  }
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.shoppingListService.addIngredientsSL(ingredients);
   }
